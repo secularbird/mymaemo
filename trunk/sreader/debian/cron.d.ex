09c150b2e067ad58bcad278c402a5d6a82a888bd
secularbird.eagle <secularbird.eagle@7f430ab2-f106-267a-a685-bdefc939f771>
2010-06-14 12:14:43 +0000

    delete old one

--- trunk/sreader/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the sreader package
-#
-0 4	* * *	root	sreader_maintenance
--- trunk/sreader/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(sreader):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="sreader" command="/usr/bin/sreader"
